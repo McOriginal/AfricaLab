2d766832cf546bfb663bb6a7f054101f9142dddb
McOriginal <originalmc223@gmail.com>
2025-04-09 23:41:36 +0000

    Finalisation de projet

---
+++ js/script.js
@@ -0,0 +1,18 @@
+AOS.init(); // initialise les animations
+
+$(function () {
+  $('.nav-btn').on('click', function () {
+    $(this).toggleClass('open');
+  });
+});
+
+$(window).ready(function () {
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll > 100) {
+      $('#header').addClass('glass-effect');
+    } else {
+      $('#header').removeClass('glass-effect');
+    }
+  });
+});
